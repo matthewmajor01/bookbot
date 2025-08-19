30bbd1b1ccd873748310190436c796ffe3661b2d
MatthewMajor <matthewmajor01@gmail.com>
2025-08-19 00:22:14 -0400

    B: Text Count

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def get_book_text (filepath):
-    
+
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
+def count_words (text):
+
+    words =  text.split()
+    num_words = len(words)
+    return num_words
+
 def main ():
 
-    print(get_book_text("./books/frankenstein.txt"))
-
+    book_text  = get_book_text("./books/frankenstein.txt")
+    num_words = count_words(book_text)
+    print(f"{num_words} words found in the document")
 main()
